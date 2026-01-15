6c6d8b7c3f2b42dbb6c140a4f5b0c9e03ff74a71
Robeel Bhatti <robeelbhatti19@gmail.com>
2026-01-15 00:02:34 -0500

    fix: create application logger using slog library (part of Go's standard library).

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,21 +4,35 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"log/slog"
 	"net/http"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "admin"
-	password = "admin"
-	dbname   = "Party"
-)
-
 type App struct {
+	log      *slog.Logger
+	httpPort int
+	dbPort   int
+	dbHost   string
+	dbName   string
+	dbUser   string
+	dbPass   string
 }
 
-func (app *App) Start() {
+// NewApp creates and returns an instance of an App struct
+// IRL would pull these values from env vars etc.
+func NewApp(log *slog.Logger) *App {
+	return &App{
+		log:      log,
+		httpPort: 8080,
+		dbPort:   5432,
+		dbHost:   "localhost",
+		dbName:   "Party",
+		dbUser:   "admin",
+		dbPass:   "admin",
+	}
+}
+
+func (app *App) Run() {
 	c := NewContainer()
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /parties/{id}", c.Pc.GetPartyById)
@@ -28,14 +42,19 @@ func (app *App) Start() {
 	mux.HandleFunc("GET /parties", c.Pc.GetParties)
 }
 
-func ConnectToDB() {
-	connInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+func (app *App) connectToDB() *sql.DB {
+	connInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		app.dbHost, app.dbPort, app.dbUser, app.dbPass, app.dbName,
+	)
 	db, err := sql.Open("postgres", connInfo)
 	if err != nil {
 		panic(err)
 	}
-}
 
-func ConnectToCache() {
-
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"robeel-bhatti/go-party-service/internal/config"
 )
 
 func main() {
-	app := &config.App{}
-	app.Start()
+	logger := configureLogger()
+	app := config.NewApp(logger)
+	app.Run()
+}
+
+func configureLogger() *slog.Logger {
+	var level slog.Level
+	val := os.Getenv("LOG_LEVEL")
+
+	switch val {
+	case "debug":
+		level = slog.LevelDebug
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	opts := &slog.HandlerOptions{Level: level}
+	return slog.New(slog.NewJSONHandler(os.Stdout, opts)).With("service", os.Getenv("SERVICE_NAME"))
 }
